4327b51df714b6a773502f62045321589f2655d6
Bunyamin <86684653+Rustemov02@users.noreply.github.com>
2023-03-08 08:13:42 +0400

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -3,22 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Stack } from '@mui/material';
 
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import img1 from './images/img-1.jpg'
-// import img2 from './images/img-2.jpg'
-// import img3 from './images/img-3.jpg'
+import "slick-carousel/slick/slick-theme.css"; 
 
 import Header from './Header';
-import Bestselling from './Bestselling';
-// import Slide from './List';
-// import Bestselling from './Bestselling';
-// import Feature from './Feature';
-// import Release from './Releases';
-// import Authors from './Authors';
-// import CustomArrows from './learnMU';
-// import Newsletter from './Newsletter';
-// import Footer from './Footer';
-// import BasicButton from './learnMU'; 
+import Bestselling from './Bestselling'; 
+
 function App() {
   
   return (
@@ -28,50 +17,4 @@ function App() {
   );
 }
 
-export default App;
-
-
-/* 
- // {
-        //     "id" : 1,
-        //     "title": "LESSONS IN CHEMISTRY",
-        //     "author": "Bonnie Garmus",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9780385547345.jpg"
-        // },
-        // {
-        //     "id" : 2,
-        //     "title": "WITHOUT A TRACE",
-        //     "author": "Danielle Steel",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9781984821867.jpg"
-        // },
-        // {
-        //     "id" : 3,
-        //     "title": "THE SEVEN HUSBANDS OF EVELYN HUGO",
-        //     "author": "Taylor Jenkins Reid",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9781501161933.jpg"
-        // },
-        // {
-        //     "id" : 4,
-        //     "title": "THE HOUSE IN THE PINES",
-        //     "author": "Ana Reyes",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9780593186718.jpg"
-        // },
-        // {
-        //     "id" : 5,
-        //     "title": "THE SILENT PATIENT",
-        //     "author": "Alex Michaelides",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9781250301697.jpg"
-        // },
-        // {
-        //     "id" : 6,
-        //     "title": "DEMON COPPERHEAD",
-        //     "author": "Barbara Kingsolver",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9780063251922.jpg"
-        // },
-        // {
-        //     "id" : 7,
-        //     "title": "LOATHE TO LOVE YOU",
-        //     "author": "Ali Hazelwood",
-        //     "book_image": "https://storage.googleapis.com/du-prd/books/images/9780593437803.jpg"
-        // }    
-*/
+export default App; 
